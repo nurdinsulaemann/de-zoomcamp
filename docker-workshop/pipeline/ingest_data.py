e35882e380d020f94f49ae044eafcce3584a3997
Nurdin <nurdinsulaeman28@gmail.com>
2026-05-16 23:39:16 +0700

    ingest data pipeline

---
+++ docker-workshop/pipeline/ingest_data.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+
+
+pg_user = 'root'
+pg_pass = 'root'
+pg_host = 'localhost'
+pg_db = 'ny_taxi'
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+engine = create_engine('postgresql+psycopg://root:root@localhost:5432/ny_taxi')
+
+def run():
+    year = 2021
+    month = 1
+    chunksize = 100000
+    target_table = 'yellow_taxi_data'
+
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+    engine = create_engine('postgresql+psycopg://root:root@localhost:5432/ny_taxi')
+
+    df_iter = pd.read_csv(
+        prefix + 'yellow_tripdata_2021-01.csv.gz',
+        dtype=dtype,
+        parse_dates=parse_dates,
+        chunksize=chunksize,
+        iterator= True
+    )
+
+    first = True
+    for df_chunk in tqdm(df_iter):
+        if first:
+            df_chunk.head(0).to_sql(
+                name = target_table, 
+                con=engine, 
+                if_exists='replace'
+                )
+        df_chunk.to_sql(
+            name=target_table, 
+            con=engine, 
+            if_exists='append'
+            )
+
+
+if __name__== '__main__':
+    run()
